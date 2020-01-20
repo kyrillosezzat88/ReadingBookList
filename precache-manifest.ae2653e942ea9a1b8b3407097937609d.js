ccd7f7029e4368ceb827240a90e21cb82626ed36
kyrillos <kyrillos417@gmail.com>
2020-01-19 21:09:39 -0800

    Updates

---
+++ precache-manifest.ae2653e942ea9a1b8b3407097937609d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba62fe6d5eb3256e48a9c3612fb43a7c",
+    "url": "/ReadingBookList/index.html"
+  },
+  {
+    "revision": "c26ad85f311bb9fdc616",
+    "url": "/ReadingBookList/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6ab4ba90ac48e02ff907",
+    "url": "/ReadingBookList/static/css/main.fe9f3fd9.chunk.css"
+  },
+  {
+    "revision": "c26ad85f311bb9fdc616",
+    "url": "/ReadingBookList/static/js/2.b263511c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReadingBookList/static/js/2.b263511c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ab4ba90ac48e02ff907",
+    "url": "/ReadingBookList/static/js/main.1b90ad36.chunk.js"
+  },
+  {
+    "revision": "c4c97515cdbbf6bc72a8",
+    "url": "/ReadingBookList/static/js/runtime-main.98a42d8a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReadingBookList/precache-manifest.ae2653e942ea9a1b8b3407097937609d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReadingBookList/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
